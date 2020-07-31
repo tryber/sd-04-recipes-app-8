97d841ae75823cf4856716e04f22630a098d09bf
Thabata <dornelasthabata@gmail.com>
2020-07-31 17:39:12 -0300

    Testando RecipeDetails

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,30 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-let finished = [{ id: 'nothing', doneDate: '' }];
-if (doneRecipes) finished = doneRecipes;
+// const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
 
-const RecipeDetails = () => {
-  const {
-    error,
-    loading,
-    mealDetailData,
-    drinkDetailData,
-    choice,
-  } = useContext(RecipeAppContext);
+// const RecipeDetails = () => {
+// FAZER
+//   return (
+//     <div className="recipe-details-page">
+//       {!loading && error && <h4>{error}</h4>}
+//       {renderDetailsPage(dataArr, choice, ingredientsArr, finished, started)}
+//     </div>
+//   );
+// };
 
-  const dataHelper = choice === 'meal' ? mealDetailData : drinkDetailData;
-  const dataArr = dataDealer(choice, dataHelper);
-  const ingredientsArr = listIngredients(dataHelper);
-  const finished = finishedArr.some((elem) => elem.id === dataArr.id);
-  const started = checkStarted(dataArr.id, choice);
-
-  return (
-    <div className="recipe-details-page">
-      {!loading && error && <h4>{error}</h4>}
-      {renderDetailsPage(dataArr, choice, ingredientsArr, finished, started)}
-    </div>
-  );
-};
-
-export default RecipeDetails;
+// export default RecipeDetails;
