f968079a28f7f5d4d571ea6e00ab4bcd4b16a3b1
Thabata <dornelasthabata@gmail.com>
2020-07-31 17:42:01 -0300

    Teste

--- src/context/provider.js
+++ src/context/provider.js
@@ -1,53 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import RecipeAppContext from './context';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import RecipeAppContext from './context';
 
-const context = {
-    mealsToken,
-    cocktailsToken,
-    setMealsToken,
-    setCocktailsToken,
-    error,
-    setError,
-    loading,
-    setLoading,
-    fetchMealID,
-    fetchDrinkID,
-    mealDetailData,
-    setMealDetailData,
-    drinkDetailData,
-    setDrinkDetailData,
-    favorite,
-    setFavorite,
-    choice,
-    setChoice,
-    mealsData,
-    beverageData,
-    basicMealData,
-    setBasicMealData,
-    basicDrikData,
-    setBasicDrikData,
-    fetchBasicMeal,
-    fetchBasicDrink,
-    ingredients,
-    setIngredients,
-    finished,
-    setFinished,
-    foodAreaFilter,
-    setFoodAreaFilter,
-    list,
-    setList,
-  };
+// const context = {
+// X
+//   };
 
-const RecipeAppProvider = ( children ) => {
-// fazer
-  return (
-    <RecipeAppContext.Provider value={context}>
-      {children}
-    </RecipeAppContext.Provider>
-  );
-};
+// const RecipeAppProvider = ( children ) => {
+// // fazer
+//   return (
+//     <RecipeAppContext.Provider value={context}>
+//       {children}
+//     </RecipeAppContext.Provider>
+//   );
+// };
 
-RecipeAppProvider.propTypes = { children: PropTypes.node.isRequired };
+// RecipeAppProvider.propTypes = { children: PropTypes.node.isRequired };
 
-export default RecipeAppProvider;
+// export default RecipeAppProvider;
