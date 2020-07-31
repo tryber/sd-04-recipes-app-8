ccca4d1fb256af7a42acad2f48a398d44da06010
frederico-campello <fredfmc33@gmail.com>
2020-07-31 10:03:28 -0300

    CC

--- src/components/HeaderFoods.jsx
+++ src/components/HeaderFoods.jsx
@@ -4,20 +4,14 @@ import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-const HeaderFoods = () => {
-  return (
-    <header>
-      <Link to="/perfil">
-        <img
-          src={profileIcon}
-          alt="Profile Icon"
-          data-testid="profile-top-btn"
-        />
-      </Link>
-      <h3 data-testid="page-title">Comidas</h3>
-      <img src={searchIcon} alt="Search Icon" data-testid="search-top-btn" />
-    </header>
-  );
-};
+const HeaderFoods = () => (
+  <header>
+    <Link to="/perfil">
+      <img src={profileIcon} alt="Profile Icon" data-testid="profile-top-btn" />
+    </Link>
+    <h3 data-testid="page-title">Comidas</h3>
+    <img src={searchIcon} alt="Search Icon" data-testid="search-top-btn" />
+  </header>
+);
 
 export default HeaderFoods;
