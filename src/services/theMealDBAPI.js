d69bfe9dd0272337d95a1d9d162cbd0375c83731
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-01 11:19:51 -0300

    merge master CC01

--- src/services/theMealDBAPI.js
+++ src/services/theMealDBAPI.js
@@ -1,10 +1,8 @@
-export async function getRecipesAPI(url) {
+export default async function getRecipesAPI(url) {
   console.log('API getbyRecipes', url);
   return fetch(url).then((recipes) =>
     recipes
       .json()
-      .then((json) =>
-        recipes.ok ? Promise.resolve(json) : Promise.reject(json),
-      ),
+      .then((json) => recipes.ok ? Promise.resolve(json) : Promise.reject(json)),
   );
 }
