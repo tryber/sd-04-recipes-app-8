ef4557578798a6972f27bc4b63d16cb346198c68
Thabata <dornelasthabata@gmail.com>
2020-08-03 23:35:54 -0300

    Btns

--- src/components/FavoriteBtn.jsx
+++ src/components/FavoriteBtn.jsx
@@ -1,12 +1,56 @@
-// import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-// import blackHeartIcon from '../images/blackHeartIcon.svg';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-// const FavoriteBtn = ({  }) => {
-// };
+const Favorite = (recipes, setFavoriteIcon) => {
+  const { id, type, area, category, alcoholicOrNot, name, image } = recipes;
+  const newFavorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+  const typeObj = {
+    Drink: 'bebida',
+    Meal: 'comida',
+};
 
-// FavoriteBtn.propTypes = {
-// };
+const favoriteIndex = newFavorites.findIndex((favorite) => favorite.id === recipes.id);
+if (favoriteIndex === -1) {
+  newFavorites.push({
+    id,
+    type: typeObj[type],
+    area: area || '',
+    category: category || '',
+    alcoholicOrNot: alcoholicOrNot || '',
+    name,
+    image,
+    });
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
+    setFavoriteIcon(blackHeartIcon);
+  } else {
+    newFavorites.splice(favoriteIndex, 1);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
+    setFavoriteIcon(whiteHeartIcon);
+  }
+};
 
-// export default FavoriteBtn;
+const FavoriteBtn = ({ dataTestId, recipes }) => {
+  const [favoriteIcon, setFavoriteIcon] = useState(whiteHeartIcon);
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    if (
+      favorites.find((favorite) => favorite.id === recipes.id)
+    ) {
+      setFavoriteIcon(blackHeartIcon);
+    }
+  }, [recipes]);
+  return (
+    <button className="invisible-btn" onClick={() => Favorite(recipes, setFavoriteIcon)}>
+      <img data-testid={dataTestId} src={favoriteIcon} alt="share" className="ruby-filter" />
+    </button>
+  );
+};
+
+FavoriteBtn.propTypes = {
+  dataTestId: PropTypes.string.isRequired,
+  recipes: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default FavoriteBtn;
--- src/components/ShareBtn.jsx
+++ src/components/ShareBtn.jsx
@@ -1,11 +1,29 @@
-// import React, { useState, Fragment } from 'react';
-// import PropTypes from 'prop-types';
-// import shareIcon from '../images/shareIcon.svg';
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
 
-// const ShareBtn = ({  }) => {
-// };
+const ShareBtn = ({ dataTestId, type, id }) => {
+  const [shareState, setShareState] = useState('');
+  return (
+    <Fragment>
+      <button
+        className="invisible-btn"
+        onClick={() => {
+          navigator.clipboard.writeText(`${window.location.origin}/${type}s/${id}`);
+          setShareState('Link copiado!');
+        }}
+      >
+        <img data-testid={dataTestId} src={shareIcon} alt="share" className="blue-filter" />
+      </button>
+      {shareState}
+    </Fragment>
+  );
+};
 
-// ShareBtn.propTypes = {
-// };
+ShareBtn.propTypes = {
+  dataTestId: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
-// export default ShareBtn;
+export default ShareBtn;
