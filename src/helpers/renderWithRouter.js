e7aaaa6488a30796986f1595eda6e0c4e0542c4d
Tiago-ers <trsilva284@gmail.com>
2020-08-05 18:09:38 -0300

    correcao renderWithRouter

--- src/helpers/renderWithRouter.js
+++ src/helpers/renderWithRouter.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 export default function renderWithRouter(component, routeConfigs = {}) {
   const route = routeConfigs.route || '/';
