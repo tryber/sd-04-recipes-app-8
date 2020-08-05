f370e366aeae1599a28af60870b2b25e27d8aace
Thabata <dornelasthabata@gmail.com>
2020-08-05 15:52:12 -0300

    DetailsPage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -4,13 +4,15 @@ import { connect } from 'react-redux';
 // import ShareBtn from '../components/ShareBtn';
 // import FavoriteBtn from '../components/FavoriteBtn';
 
-const DetailsPage = ({ recipes }) => (
+const DetailsPage = ({ recipes }) => {
+  // const { recipes } = useSelector((state) => state.ThemealDB);
+  // const id = searchSetting.recipeId;
   <div>
     {/* <h1>{recipes.strMeal}</h1>
     <h2>{recipes.idMeal}</h2> */}
-    <h1>Teste Detail</h1>
+    <h1>Título</h1>
   </div>
-);
+};
 
 const mapStateToProps = (state) => ({
   recipes: state.ThemealDB.recipes,
