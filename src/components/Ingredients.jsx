e4b8407ef5e9fea108a5c1d9fa9f2da2c41c932a
frederico-campello <fredfmc33@gmail.com>
2020-08-07 19:12:23 -0300

    Ingredients

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -2,7 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const Ingredients = ({ recipe }) => <p>{recipe[0].strIngredient1}</p>;
+const Ingredients = ({ recipe }) => {
+  let filteredIngredients = [];
+  let filteredMeasures = [];
+  Object.keys(recipe[0]).forEach(function (item) {
+    if (
+      item.includes('strIngredient') &
+      (recipe[0][item] !== '') &
+      (recipe[0][item] !== null)
+    ) {
+      filteredIngredients.push(recipe[0][item]);
+    }
+    if (
+      item.includes('strMeasure') &
+      (recipe[0][item] !== ' ') &
+      (recipe[0][item] !== null)
+    ) {
+      filteredMeasures.push(recipe[0][item]);
+    }
+  });
+
+  let ingredients = {};
+  filteredIngredients.forEach(
+    (ingr, i) => (ingredients[ingr] = filteredMeasures[i]),
+  );
+
+  return Object.entries(ingredients).map((item, index) => (
+    <p
+      data-testid={`${index}-ingredient-and-measure`}
+    >{`- ${item[0]} - ${item[1]}`}</p>
+  ));
+};
 
 const mapStateToProps = (state) => ({
   recipe: state.DetailReducer.id,
