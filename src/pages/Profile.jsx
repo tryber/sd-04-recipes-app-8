7932a8e26869323fbf359f57b6edc237b3e86a50
frederico-campello <fredfmc33@gmail.com>
2020-08-07 22:12:19 -0300

    Corrigindo Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Footer from '../components/Footer';
@@ -8,21 +8,13 @@ import profileIcon from '../images/profileIcon.svg';
 
 import './Profile.css';
 
-// const [email, setEmail] = useState('');
-
-// useEffect(() => {
-//   setEmail(JSON.parse(localStorage.getItem('user')).email);
-// }, []);
-
-const Profile = () => (
+const Profile = ({ email }) => (
   <div>
     <header className="header-profile">
       <img src={profileIcon} alt="Profile Icon" data-testid="profile-top-btn" />
       <h3 data-testid="page-title">Perfil</h3>
     </header>
-    <p data-testid="profile-email">
-      {JSON.parse(localStorage.getItem('user')).email}
-    </p>
+    <p data-testid="profile-email">{email}</p>
     <Link to="/receitas-feitas">
       <button type="button" data-testid="profile-done-btn">
         Receitas Feitas
@@ -45,15 +37,12 @@ const Profile = () => (
     <Footer />
   </div>
 );
+const mapStateToProps = (state) => ({
+  email: state.saveEmail.email,
+});
 
-// const mapStateToProps = (state) => ({
-//   email: state.saveEmail.email,
-// });
+Profile.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
-// Profile.propTypes = {
-//   email: PropTypes.string.isRequired,
-// };
-
-// export default connect(mapStateToProps, null)(Profile);
-
-export default Profile;
+export default connect(mapStateToProps, null)(Profile);
