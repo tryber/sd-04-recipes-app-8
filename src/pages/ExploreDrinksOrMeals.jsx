58ff3a34c0ce93ed98a4c8c4d1eef76f7251dc37
frederico-campello <fredfmc33@gmail.com>
2020-08-10 21:09:13 -0300

    Explore Random

--- src/pages/ExploreDrinksOrMeals.jsx
+++ src/pages/ExploreDrinksOrMeals.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch, connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import ExploreHeader from '../components/ExploreHeader';
 import Footer from '../components/Footer';
 
-const ExploreDrinksOrMeals = () => {
-  const currentLocation = window.location.pathname;
+import FetchRandomAPI from '../actions/RandomActions';
+
+const ExploreDrinksOrMeals = ({ id, currentLocation }) => {
+  const dispatch = useDispatch();
   const surprise = window.location.pathname.slice(9, 17);
-  const id = '123456789'; // substituir pelo id da api random
+  const recipeType = currentLocation === '/comidas' ? 'Meal' : 'Drink';
   const headerTitle =
     currentLocation === '/explorar/comidas'
       ? 'Explorar Comidas'
       : 'Explorar Bebidas';
+
+  if (id.length > 0)
+    return <Redirect to={`${surprise}/${id[0][`id${recipeType}`]}`} />;
 
   return (
     <div>
@@ -28,14 +35,26 @@ const ExploreDrinksOrMeals = () => {
           </button>
         </Link>
       )}
-      <Link to={`${surprise}/${id}`}>
-        <button type="button" data-testid="explore-surprise">
-          Me Surpreenda!
-        </button>
-      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={() => dispatch(FetchRandomAPI())}
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
 };
 
-export default ExploreDrinksOrMeals;
+const mapStateToProps = (state) => ({
+  id: state.DetailReducer.id,
+  currentLocation: state.updateLocation.currentLocation,
+});
+
+ExploreDrinksOrMeals.propTypes = {
+  id: PropTypes.arrayOf(PropTypes.string).isRequired,
+  currentLocation: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(ExploreDrinksOrMeals);
